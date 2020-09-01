7c974654fd128bf94cef6b4fc9222a887f82ffa2
jvisser <jvisser3@gmail.com>
2020-09-01 13:15:19 +0200

    Move buffer related code from SLZCompressor to SLZCompressionBuffer

--- tools/mapconvert/src/main/java/com/ultradrive/mapconvert/export/compression/slz/SLZCompressionBuffer.java
+++ tools/mapconvert/src/main/java/com/ultradrive/mapconvert/export/compression/slz/SLZCompressionBuffer.java
@@ -9,6 +9,7 @@ class SLZCompressionBuffer
 {
     private static final int MAX_TOKENS = 8;
 
+    private final List<Byte> compressedResult = new ArrayList<>();
     private final List<Byte> tokenBuffer = new ArrayList<>();
 
     private BitPacker tokenCompressionMarkers = new BitPacker(MAX_TOKENS);
@@ -18,41 +19,26 @@ class SLZCompressionBuffer
         token.write(tokenBuffer);
 
         tokenCompressionMarkers = tokenCompressionMarkers.insert(token.isCompressed());
-    }
-
-    public List<Byte> complete()
-    {
-        List<Byte> compressedBytes = new ArrayList<>();
-
-        if (!isFull())
+        if (tokenCompressionMarkers.isFull())
         {
-            tokenCompressionMarkers = tokenCompressionMarkers.padStart(MAX_TOKENS - tokenCompressionMarkers.getSize());
-
-            compressedBytes.add(tokenCompressionMarkers.byteValue());
-            compressedBytes.addAll(tokenBuffer);
-        }
-
-        return compressedBytes;
-    }
-
-    public boolean isFull()
-    {
-        return tokenCompressionMarkers.isFull();
-    }
-
-    public List<Byte> reset()
-    {
-        List<Byte> compressedBytes = new ArrayList<>();
-
-        if (isFull())
-        {
-            compressedBytes.add(tokenCompressionMarkers.byteValue());
-            compressedBytes.addAll(tokenBuffer);
+            compressedResult.add(tokenCompressionMarkers.byteValue());
+            compressedResult.addAll(tokenBuffer);
 
             tokenBuffer.clear();
             tokenCompressionMarkers = new BitPacker(MAX_TOKENS);
         }
+    }
 
-        return compressedBytes;
+    public List<Byte> complete()
+    {
+        if (!tokenCompressionMarkers.isFull())
+        {
+            tokenCompressionMarkers = tokenCompressionMarkers.padStart(MAX_TOKENS - tokenCompressionMarkers.getSize());
+
+            compressedResult.add(tokenCompressionMarkers.byteValue());
+            compressedResult.addAll(tokenBuffer);
+        }
+
+        return compressedResult;
     }
 }
