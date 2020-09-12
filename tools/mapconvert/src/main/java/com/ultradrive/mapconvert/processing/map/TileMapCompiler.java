882d63505e0396fe979766184755c82ac20f9b11
jvisser <946768+jvisser@users.noreply.github.com>
2020-09-12 13:15:39 +0200

    Process auxiliary map references

--- tools/mapconvert/src/main/java/com/ultradrive/mapconvert/processing/map/TileMapCompiler.java
+++ tools/mapconvert/src/main/java/com/ultradrive/mapconvert/processing/map/TileMapCompiler.java
@@ -1,5 +1,6 @@
 package com.ultradrive.mapconvert.processing.map;
 
+import com.google.common.collect.Sets;
 import com.ultradrive.mapconvert.datasource.MapDataSource;
 import com.ultradrive.mapconvert.datasource.TilesetDataSource;
 import com.ultradrive.mapconvert.processing.tileset.Tileset;
@@ -11,7 +12,6 @@ import java.util.Map;
 import java.util.Set;
 
 import static java.util.List.copyOf;
-import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 
 
@@ -36,7 +36,9 @@ public class TileMapCompiler
         Map<TilesetDataSource, TilesetBuilder> tilesetBuildersByTilesetDataSource = new HashMap<>();
         Map<MapDataSource, TileMapBuilder> mapBuilders = new HashMap<>();
 
-        for (MapDataSource mapDataSource : mapDataSources)
+        Map<MapDataSource, Set<AuxiliaryMapSource<MapDataSource>>> auxiliaryMaps = collectAuxiliaryMaps();
+
+        for (MapDataSource mapDataSource : Sets.union(mapDataSources, auxiliaryMaps.keySet()))
         {
             TilesetDataSource tilesetDataSource = mapDataSource.getTilesetDataSource();
             TilesetBuilder tilesetBuilder = tilesetBuildersByTilesetDataSource
@@ -50,9 +52,36 @@ public class TileMapCompiler
 
         Map<TilesetDataSource, Tileset> tilesetsByTilesetDataSource =
                 compileTilesets(tilesetBuildersByTilesetDataSource);
-        List<TileMap> maps = compileMaps(mapBuilders, tilesetsByTilesetDataSource);
+
+        List<TileMap> maps = bindAuxiliaryMaps(auxiliaryMaps, compileMaps(mapBuilders, tilesetsByTilesetDataSource));
 
         return new TileMapCompilation(maps, copyOf(tilesetsByTilesetDataSource.values()));
+    }
+
+    private Map<MapDataSource, Set<AuxiliaryMapSource<MapDataSource>>> collectAuxiliaryMaps()
+    {
+        Map<MapDataSource, Set<AuxiliaryMapSource<MapDataSource>>> collector = new HashMap<>();
+
+        mapDataSources.forEach(source -> collectAuxiliaryMaps(collector, source));
+
+        return collector;
+    }
+
+    private void collectAuxiliaryMaps(Map<MapDataSource, Set<AuxiliaryMapSource<MapDataSource>>> collector, MapDataSource source)
+    {
+        for (Map.Entry<String, Object> propertyEntry : source.getProperties().entrySet())
+        {
+            if (propertyEntry.getValue() instanceof MapDataSource auxiliaryMapDataSource)
+            {
+                collector.computeIfAbsent(auxiliaryMapDataSource, auxMapSource -> new HashSet<>())
+                        .add(new AuxiliaryMapSource<>(source, propertyEntry.getKey()));
+
+                if (!collector.containsKey(auxiliaryMapDataSource))
+                {
+                    collectAuxiliaryMaps(collector, auxiliaryMapDataSource);
+                }
+            }
+        }
     }
 
     private void preCompile(Map<MapDataSource, TileMapBuilder> mapBuilders)
@@ -60,24 +89,45 @@ public class TileMapCompiler
         mapBuilders.values().forEach(TileMapBuilder::collectReferences);
     }
 
-    private List<TileMap> compileMaps(Map<MapDataSource, TileMapBuilder> mapBuilders,
-                                      Map<TilesetDataSource, Tileset> tilesetsByTilesetDataSource)
-    {
-        return mapBuilders.entrySet().stream()
-                .map(mapBuilderEntry ->
-                     {
-                         MapDataSource mapDataSource = mapBuilderEntry.getKey();
-                         TileMapBuilder mapBuilder = mapBuilderEntry.getValue();
-
-                         return mapBuilder.build(tilesetsByTilesetDataSource.get(mapDataSource.getTilesetDataSource()));
-                     })
-                .collect(toList());
-    }
-
     private Map<TilesetDataSource, Tileset> compileTilesets(
             Map<TilesetDataSource, TilesetBuilder> tilesetBuildersByTilesetDataSource)
     {
         return tilesetBuildersByTilesetDataSource.entrySet().stream()
                 .collect(toMap(Map.Entry::getKey, e -> e.getValue().compile()));
+    }
+
+    private Map<MapDataSource, TileMap> compileMaps(Map<MapDataSource, TileMapBuilder> mapBuilders,
+                                      Map<TilesetDataSource, Tileset> tilesetsByTilesetDataSource)
+    {
+        return mapBuilders.entrySet().stream()
+                .collect(toMap(Map.Entry::getKey, mapBuilderEntry ->
+                     {
+                         MapDataSource mapDataSource = mapBuilderEntry.getKey();
+                         TileMapBuilder mapBuilder = mapBuilderEntry.getValue();
+
+                         return mapBuilder.build(tilesetsByTilesetDataSource.get(mapDataSource.getTilesetDataSource()));
+                     }));
+    }
+
+    private List<TileMap> bindAuxiliaryMaps(Map<MapDataSource, Set<AuxiliaryMapSource<MapDataSource>>> auxiliaryMaps,
+                                            Map<MapDataSource, TileMap> compiledMaps)
+    {
+        Map<MapDataSource, TileMap> resultMaps = new HashMap<>(compiledMaps);
+
+        auxiliaryMaps.forEach((mapDataSource, auxiliaryMapSources) -> {
+            TileMap compiledAuxiliaryMap = compiledMaps.get(mapDataSource);
+
+            auxiliaryMapSources.forEach(source -> {
+                MapDataSource mapSourceToRewriteProperties = source.getSource();
+
+                Map<String, Object> rewrittenProperties = new HashMap<>(mapSourceToRewriteProperties.getProperties());
+                rewrittenProperties.put(source.getPropertyName(), compiledAuxiliaryMap);
+
+                resultMaps.put(mapSourceToRewriteProperties,
+                               compiledMaps.get(mapSourceToRewriteProperties).withProperties(rewrittenProperties));
+            });
+        });
+
+        return List.copyOf(resultMaps.values());
     }
 }
