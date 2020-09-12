225caa58ffc9ccf918e2e85c350cb58b95dd6b0c
Jeroen <maleboluga>
2020-09-12 17:57:16 +0200

    Expose tileset properties to export template

--- tools/mapconvert/src/main/java/com/ultradrive/mapconvert/processing/tileset/Tileset.java
+++ tools/mapconvert/src/main/java/com/ultradrive/mapconvert/processing/tileset/Tileset.java
@@ -1,5 +1,6 @@
 package com.ultradrive.mapconvert.processing.tileset;
 
+import com.ultradrive.mapconvert.common.PropertySource;
 import com.ultradrive.mapconvert.processing.tileset.block.Block;
 import com.ultradrive.mapconvert.processing.tileset.block.BlockTileset;
 import com.ultradrive.mapconvert.processing.tileset.block.image.TilesetImagePalette;
@@ -8,22 +9,32 @@ import com.ultradrive.mapconvert.processing.tileset.chunk.Chunk;
 import com.ultradrive.mapconvert.processing.tileset.chunk.ChunkTileset;
 import com.ultradrive.mapconvert.processing.tileset.collision.CollisionFieldSet;
 import com.ultradrive.mapconvert.processing.tileset.common.MetaTileMetrics;
+import java.util.Map;
 
 
-public class Tileset
+public class Tileset implements PropertySource
 {
     private final ChunkTileset chunkTileset;
     private final BlockTileset blockTileset;
     private final CollisionFieldSet collisionFieldSet;
+    private final Map<String, Object> properties;
 
     private final String name;
 
-    public Tileset(ChunkTileset chunkTileset, BlockTileset blockTileset, CollisionFieldSet collisionFieldSet, String name)
+    public Tileset(ChunkTileset chunkTileset, BlockTileset blockTileset, CollisionFieldSet collisionFieldSet,
+                   Map<String, Object> properties, String name)
     {
         this.chunkTileset = chunkTileset;
         this.blockTileset = blockTileset;
         this.collisionFieldSet = collisionFieldSet;
+        this.properties = properties;
         this.name = name;
+    }
+
+    @Override
+    public Map<String, Object> getProperties()
+    {
+        return properties;
     }
 
     public ChunkTileset getChunkTileset()
