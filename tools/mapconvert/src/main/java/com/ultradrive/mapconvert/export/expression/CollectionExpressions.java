a581237adeb8f15e6822cb97e745627be62b78ba
Jeroen <maleboluga>
2020-09-12 17:03:51 +0200

    Add skip/take iterables

--- tools/mapconvert/src/main/java/com/ultradrive/mapconvert/export/expression/CollectionExpressions.java
+++ tools/mapconvert/src/main/java/com/ultradrive/mapconvert/export/expression/CollectionExpressions.java
@@ -4,11 +4,13 @@ package com.ultradrive.mapconvert.export.expression;
 import com.ultradrive.mapconvert.common.collection.iterables.ConcatenatingIterable;
 import com.ultradrive.mapconvert.common.collection.iterables.FlatteningIterable;
 import com.ultradrive.mapconvert.common.collection.iterables.GroupingIterable;
+import com.ultradrive.mapconvert.common.collection.iterables.SkipIterable;
+import com.ultradrive.mapconvert.common.collection.iterables.TakeIterable;
 
 
 public class CollectionExpressions
 {
-    public <T> Iterable<Iterable<T>> group(Iterable<T> iterableIterable, int groupSize)
+    public <T> Iterable<Iterable<T>> group(int groupSize, Iterable<T> iterableIterable)
     {
         return new GroupingIterable<>(iterableIterable, groupSize);
     }
@@ -21,5 +23,15 @@ public class CollectionExpressions
     public <T> Iterable<T> concat(Iterable<T> first, Iterable<T> second)
     {
         return new ConcatenatingIterable<>(first, second);
+    }
+
+    public <T> Iterable<T> skip(int skipCount, Iterable<T> iterable)
+    {
+        return new SkipIterable<>(iterable, skipCount);
+    }
+
+    public <T> Iterable<T> take(int takeCount, Iterable<T> iterable)
+    {
+        return new TakeIterable<>(iterable, takeCount);
     }
 }
