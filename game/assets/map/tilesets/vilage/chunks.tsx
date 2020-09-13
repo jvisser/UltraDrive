14b08a60c044f8305b356933c63b755bbb16fda1
Jeroen <maleboluga>
2020-09-13 12:49:19 +0200

    Add Tiled test map

---
+++ game/assets/map/tilesets/vilage/chunks.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="chunks" tilewidth="128" tileheight="128" tilecount="12" columns="6">
+ <image source="chunks.tmx" width="768" height="256"/>
+ <tile id="0" type="chunk"/>
+ <tile id="1" type="chunk"/>
+ <tile id="2" type="chunk"/>
+ <tile id="3" type="chunk"/>
+ <tile id="4" type="chunk"/>
+ <tile id="5" type="chunk"/>
+ <tile id="6" type="chunk"/>
+ <tile id="7" type="chunk"/>
+ <tile id="8" type="chunk"/>
+ <tile id="9" type="chunk"/>
+ <tile id="10" type="chunk"/>
+ <tile id="11" type="chunk"/>
+</tileset>
