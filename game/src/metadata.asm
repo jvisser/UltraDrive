52dc63ed54887d8756cec13d7eeb65a8b7280059
jvisser <946768+jvisser@users.noreply.github.com>
2020-09-15 21:11:44 +0200

    Initial commit game code

---
+++ game/src/metadata.asm
@@ -0,0 +1,11 @@
+;------------------------------------------------------------------------------------------
+; Game meta data
+;------------------------------------------------------------------------------------------
+
+TITLE           equs 'UltraDrive'
+COPYRIGHT       equs '(C)JV'
+RELEASE_DATE    equs '2020.SEPT'
+SERIAL_NUMBER   equs '13371337'
+REVISION        equs '00'
+REGION          equs 'JUE'
+DEVICE_SUPPORT  equs 'J'
