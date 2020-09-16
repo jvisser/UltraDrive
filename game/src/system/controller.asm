1b55c33f7e2103ef452661475f1de622a7ff81d2
Jeroen <maleboluga>
2020-09-16 22:29:49 +0200

    Init system

--- game/src/system/controller.asm
+++ game/src/system/controller.asm
@@ -2,9 +2,14 @@
 ; Controller code
 ;------------------------------------------------------------------------------------------
 
-MEM_CTRL1_DATA  equ $00a10002
-MEM_CTRL2_DATA  equ $00a10004
-MEM_CTRL3_DATA  equ $00a10006   ; Back expansion port
-MEM_CTRL1_CTRL  equ $00a10008
-MEM_CTRL2_CTRL  equ $00a1000a
-MEM_CTRL3_CTRL  equ $00a1000c   ; Back expansion port
+;-------------------------------------------------
+; Controller related 68000 memory addresses
+; ----------------
+MEM_CTRL1_DATA  Equ $00a10002
+MEM_CTRL1_CTRL  Equ $00a10008
+
+MEM_CTRL2_DATA  Equ $00a10004
+MEM_CTRL2_CTRL  Equ $00a1000a
+
+MEM_CTRL3_DATA  Equ $00a10006   ; Back expansion port
+MEM_CTRL3_CTRL  Equ $00a1000c   ; Back expansion port
---
+++ game/src/ultradrive/metadata.asm
@@ -0,0 +1,13 @@
+;------------------------------------------------------------------------------------------
+; Program meta data. Injected in Sega ROM header.
+;------------------------------------------------------------------------------------------
+
+TITLE               Equs 'UltraDrive'
+COPYRIGHT_HOLDER    Equs 'JV'
+SERIAL_NUMBER       Equs 'GM 13371337'
+REVISION            Equs '00'
+DEVICE_SUPPORT      Equs 'J'
+REGION              Equs 'JUE'
+
+RELEASE_YEAR        Equ \#_year+1900
+RELEASE_MONTH       Equ \#_month
