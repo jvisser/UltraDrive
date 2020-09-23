5367bb720b43a2e87de2ae723195ff0d35fbe988
jvisser <946768+jvisser@users.noreply.github.com>
2020-09-23 20:19:44 +0200

    Add VDP macros for reg/addr set commands

--- game/src/system/vdpdma.asm
+++ game/src/system/vdpdma.asm
@@ -8,12 +8,12 @@
 ; DMA transfer structure
 ; ----------------
     DEFINE_STRUCT VDPDMATransfer
-        STRUCT_MEMBER w, vdpRegDMALengthLow
-        STRUCT_MEMBER w, vdpRegDMALengthHigh
-        STRUCT_MEMBER w, vdpRegDMASourceLow
-        STRUCT_MEMBER w, vdpRegDMASourceMid
-        STRUCT_MEMBER w, vdpRegDMASourceHigh
-        STRUCT_MEMBER l, vdpDMATarget
+        STRUCT_MEMBER.w vdpRegDMALengthHigh
+        STRUCT_MEMBER.w vdpRegDMALengthLow
+        STRUCT_MEMBER.w vdpRegDMASourceHigh
+        STRUCT_MEMBER.w vdpRegDMASourceMid
+        STRUCT_MEMBER.w vdpRegDMASourceLow
+        STRUCT_MEMBER.l vdpDMATarget
     DEFINE_STRUCT_END
 
 
@@ -21,11 +21,11 @@
 ; Store register values
 ; ----------------
 _VDP_STATIC_TRANSFER_SETREGS Macro source, length
-        dc.w VDP_CMD_RS_DMA_LEN_L + (\length & $ff)
         dc.w VDP_CMD_RS_DMA_LEN_H + ((\length & $ff00) >> 8)
-        dc.w VDP_CMD_RS_DMA_SRC_L + ((\source >> 1) & $ff)
-        dc.w VDP_CMD_RS_DMA_SRC_M + (((\source >> 1)& $ff00) >> 8)
+        dc.w VDP_CMD_RS_DMA_LEN_L + (\length & $ff)
         dc.w VDP_CMD_RS_DMA_SRC_H + (((\source >> 1)& $7f0000) >> 16)
+        dc.w VDP_CMD_RS_DMA_SRC_M + (((\source >> 1)& $ff00) >> 8)
+        dc.w VDP_CMD_RS_DMA_SRC_L + ((\source >> 1) & $ff)
     Endm
 
 
@@ -34,7 +34,7 @@ _VDP_STATIC_TRANSFER_SETREGS Macro source, length
 ; ----------------
 VDP_DEFINE_STATIC_DMA_VRAM_TRANSFER Macro source, target, length
         _VDP_STATIC_TRANSFER_SETREGS \source, \length
-        dc.l VDP_CMD_AS_VRAM_WRITE + VDP_CMD_AS_DMA + ((\target & $3fff) << 16) + ((\target & $c000) >> 14)
+        dc.l VDP_CMD_AS_VRAM_WRITE | VDP_CMD_AS_DMA | ((\target & $3fff) << 16) | ((\target & $c000) >> 14)
     Endm
 
 
@@ -43,7 +43,7 @@ VDP_DEFINE_STATIC_DMA_VRAM_TRANSFER Macro source, target, length
 ; ----------------
 VDP_DEFINE_STATIC_DMA_CRAM_TRANSFER Macro source, target, length
         _VDP_STATIC_TRANSFER_SETREGS \source, \length
-        dc.l VDP_CMD_AS_CRAM_WRITE + VDP_CMD_AS_DMA + (\target << 16)
+        dc.l VDP_CMD_AS_CRAM_WRITE | VDP_CMD_AS_DMA | (\target << 16)
     Endm
 
 
@@ -52,7 +52,7 @@ VDP_DEFINE_STATIC_DMA_CRAM_TRANSFER Macro source, target, length
 ; ----------------
 VDP_DEFINE_STATIC_DMA_VSRAM_TRANSFER Macro source, target, length
         _VDP_STATIC_TRANSFER_SETREGS \source, \length
-        dc.l VDP_CMD_AS_VSRAM_WRITE + VDP_CMD_AS_DMA + (\target << 16)
+        dc.l VDP_CMD_AS_VSRAM_WRITE | VDP_CMD_AS_DMA | (\target << 16)
     Endm
 
 
@@ -60,7 +60,7 @@ VDP_DEFINE_STATIC_DMA_VSRAM_TRANSFER Macro source, target, length
 ; Start a DMA transfer for the specified VDPDMATransfer
 ; Uses: a0-a1
 ; ----------------
-VDP_DMA_TRANSFER Macro vdpDMATransferBlockAddress
+VDP_STATIC_DMA_TRANSFER Macro vdpDMATransferBlockAddress
         lea     vdpDMATransferBlockAddress, a0
         lea     MEM_VDP_CTRL, a1
         move.l  (a0)+, (a1)
