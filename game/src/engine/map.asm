1fa592871286330d747f2cab3f62ba24fa9af7d5
Jeroen <maleboluga>
2020-10-04 20:29:11 +0200

    Add map loader subroutine

--- game/src/engine/map.asm
+++ game/src/engine/map.asm
@@ -12,3 +12,27 @@
         STRUCT_MEMBER.l tilesetAddress
         STRUCT_MEMBER.l rowOffsetTable
     DEFINE_STRUCT_END
+
+    DEFINE_VAR FAST
+        VAR.l       loadedMap
+    DEFINE_VAR_END
+
+
+;-------------------------------------------------
+; Load a map and its associated resources
+; ----------------
+; Input:
+; - a0: Map address
+; Uses: d0-d7/a0-a6
+MapLoad:
+        cmpa.w  loadedMap, a0
+        bne     .loadMap
+        rts ; Map already loaded
+
+    .loadMap:
+        move.l  a0, loadedMap
+
+        ; Load associated tileset
+        movea.l tilesetAddress(a0), a0
+        jsr     TilesetLoad
+        rts
