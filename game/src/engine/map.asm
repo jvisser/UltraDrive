59034f0469d74f99ccc0150b243a939ce1a84678
jvisser <jvisser3@gmail.com>
2020-10-17 14:06:46 +0200

    Add single column rendering

--- game/src/engine/map.asm
+++ game/src/engine/map.asm
@@ -19,8 +19,8 @@
 
     DEFINE_VAR FAST
         VAR.l               loadedMap
-        VAR.w               mapRowBuffer,               64 + 2 ; 2 word of overflow space. NB: Assumes scrollable plane side will never be 128.
-        VAR.w               mapColumnBuffer,            64 + 2
+        VAR.w               mapRowBuffer,               64  ; NB: Assumes scrollable plane side will never be 128.
+        VAR.w               mapColumnBuffer,            64
         VAR.VDPDMATransfer  mapRowBufferDMATransfer
         VAR.VDPDMATransfer  mapColumnBufferDMATransfer
     DEFINE_VAR_END
@@ -35,12 +35,12 @@ MapInit:
 
         move.w  #2, (mapRowBufferDMATransfer + dmaDataStride)
         move.w  d1, (mapRowBufferDMATransfer + dmaLength)
-        move.l  #((mapRowBuffer + SIZE_WORD) >> 1) & $7fffff, (mapRowBufferDMATransfer + dmaSource)
+        move.l  #(mapRowBuffer >> 1) & $7fffff, (mapRowBufferDMATransfer + dmaSource)
 
         add.w   d1, d1
         move.w  d1, (mapColumnBufferDMATransfer + dmaDataStride)
         move.w  d0, (mapColumnBufferDMATransfer + dmaLength)
-        move.l  #((mapColumnBuffer + SIZE_WORD) >> 1) & $7fffff, (mapColumnBufferDMATransfer + dmaSource)
+        move.l  #(mapColumnBuffer >> 1) & $7fffff, (mapColumnBufferDMATransfer + dmaSource)
         rts
 
 
@@ -91,6 +91,145 @@ MapRender:
 
 
 ;-------------------------------------------------
+; Shared macros between row/column renderer
+; ----------------
+
+;-------------------------------------------------
+; Render a single pattern to the render buffer
+; ----------------
+; Input:
+; - d0: Buffer mask
+; - d1: Buffer offset
+; - d4: Pattern reference
+; - d3: Orientation flags of chunk + block
+_RENDER_PATTERN Macro
+            eor.w   d3, d4                                          ; orient pattern ref by block + chunk orientation
+            and.w   d0, d1                                          ; Wrap buffer position
+            move.w  d4, (a4, d1)                                    ; Write pattern to row DMA buffer
+            addq.w  #SIZE_WORD, d1
+    Endm
+
+
+;-------------------------------------------------
+; Render a partial chunk based on the amount of patterns given
+; ----------------
+_RENDER_PARTIAL_CHUNK Macro start, patternNumber
+            If (strcmp('\start', 'START'))
+                btst    #0, \patternNumber
+                beq     .fullBlocks\@
+
+                _RENDER_BLOCK END                                   ; Only render the last pattern of the first block
+
+            ElseIf (strcmp('\start', 'END'))
+
+                PUSHW   \patternNumber                              ; Store for last block
+
+            EndIf
+
+        .fullBlocks\@:
+            lsr.w   #1, \patternNumber
+            beq     .fullBlocksDone\@
+            subq.w  #1, \patternNumber
+        .renderBlockLoop\@:
+            _RENDER_BLOCK
+            dbra \patternNumber, .renderBlockLoop\@
+        .fullBlocksDone\@:
+
+        If (strcmp('\start', 'END'))
+            POPW   \patternNumber
+
+            btst    #0, \patternNumber
+            beq     .chunkDone\@
+
+            _RENDER_BLOCK START                                     ; Only render the first pattern of the last block
+
+        EndIf
+
+        .chunkDone\@:
+    Endm
+
+
+;-------------------------------------------------
+; Render the full render buffer
+; ----------------
+; Input:
+; - a0: Plane size in patterns
+; - d2: Map start row/column (Opposite of rendered dimension)
+; Reserved registers: a2-a4
+; Required macros:
+; - _START_CHUNK
+; - _RENDER_BLOCK
+_RENDER_BUFFER Macro buffer
+            ; Load address registers
+            lea     chunkTable, a2
+            lea     blockTable, a3
+            lea     \buffer, a4
+
+            moveq   #0, d3
+            andi.w  #$0f, d2
+            beq     .fullChunks\@
+
+            ; ---------------------------------------------
+            ; Render first partial chunk
+
+            move.w  d2, d3
+            andi.w  #$0e, d3                                        ; d3 = offset of block row/column in chunk
+
+            _START_CHUNK d3
+
+            neg.w   d2
+            andi.w  #$0f, d2                                        ; d2 = number of patterns rendered
+
+            PUSHW   d2                                              ; Store for later use
+
+            _RENDER_PARTIAL_CHUNK START, d2
+
+            POPW    d3                                              ; d3 = number of patterns left to render
+
+            ; ---------------------------------------------
+            ; Render full chunks
+
+        .fullChunks\@:
+            move.w  a0, d2
+            sub.w   d3, d2                                          ; d2 = remaining pattern columns
+
+            PUSHW d2                                                ; Store for later use
+
+            lsr.w   #4, d2                                          ; d2 = number of full chunks
+
+            subq.w  #1, d2
+        .fullChunkLoop\@:
+
+                _START_CHUNK
+
+                    _RENDER_BLOCK
+                    _RENDER_BLOCK
+                    _RENDER_BLOCK
+                    _RENDER_BLOCK
+                    _RENDER_BLOCK
+                    _RENDER_BLOCK
+                    _RENDER_BLOCK
+                    _RENDER_BLOCK
+
+            dbra d2, .fullChunkLoop\@
+
+            ; ---------------------------------------------
+            ; Render the last partial chunk
+
+            POPW d2                                                 ; d2 = number of patterns left to render
+
+            andi.w  #$0f, d2
+            beq     .done\@                                         ; Nothing left
+
+            _START_CHUNK
+
+                _RENDER_PARTIAL_CHUNK END, d2
+
+        .done\@:
+    Endm
+
+
+;-------------------------------------------------
 ; Render a single row of the map at the specified plane row
 ; ----------------
 ; Input:
@@ -134,36 +273,34 @@ MapRenderRow:
 ; Uses: d0-d7/a0-a6
 _MapRenderRowBuffer:
 _START_CHUNK Macro colOffset
-            ; Get chunk row address
             move.w  (a1)+, d7                                       ; Chunk ref in d7
             move.w  d7, d5
-            and.w   #$ff, d5
             lsl.w   #7, d5
             lea     (a2, d5), a5
             swap    d6                                              ; d6 = chunk row offset
             move.w  d6, d5
             btst    #CHUNK_REF_VFLIP, d7
-            beq     .fullChunkNotVFlipped\@
+            beq     .chunkNotVFlipped\@
             eor.w   #$0070, d5                                      ; Flip chunk row offset
 
-        .fullChunkNotVFlipped\@:
+        .chunkNotVFlipped\@:
             If (narg = 1)
                 add.w  \colOffset, d5
             EndIf
             moveq   #SIZE_WORD, d4
             btst    #CHUNK_REF_HFLIP, d7
-            beq     .fullChunkNotHFlipped\@
+            beq     .chunkNotHFlipped\@
             neg.w   d4
             eor.w   #$000e, d5                                      ; Flip chunk column offset
 
-        .fullChunkNotHFlipped\@:
+        .chunkNotHFlipped\@:
             adda.w  d5, a5                                          ; a5 = chunk block address
             swap    d7
             move.w  d4, d7                                          ; d7 = [chunk reference]:[block address increment]
             swap    d6                                              ; d6 = block row index
         Endm
 
-_RENDER_BLOCK Macro
+_RENDER_BLOCK Macro position
             move.w  (a5), d3                                        ; d3 = block ref
             adda.w  d7, a5
             swap    d7
@@ -189,35 +326,29 @@ _RENDER_BLOCK Macro
             add.w   d3, d3                                          ; Allign block+chunk orientation flags with pattern orientation flags
             andi.w  #PATTERN_REF_ORIENTATION_MASK, d3
 
-_RENDER_PATTERN Macro
-                eor.w   d3, d4                                      ; orient pattern ref by block + chunk orientation
-                and.w   d0, d1                                      ; Wrap buffer position
-                move.w  d4, (a4, d1)                                ; Write pattern to row DMA buffer
-                addq.w  #SIZE_WORD, d1
-            Endm
+            If ((narg = 0) | strcmp('\position', 'START'))
+                _RENDER_PATTERN
+            EndIf
 
-            _RENDER_PATTERN
-
-            swap    d4
-
-            _RENDER_PATTERN
-
-            Purge _RENDER_PATTERN
+            If ((narg = 0) | strcmp('\position', 'END'))
+                swap    d4
+                _RENDER_PATTERN
+            EndIf
         Endm
 
-_RENDER_PARTIAL_CHUNK Macro patternNumber
-            addq.w  #1, \patternNumber                              ; In case of a partial block just render the whole block (wastes 34 cycles) (space is reserved in the DMA buffer)
-            lsr.w   #1, \patternNumber
-            subq.w  #1, \patternNumber
-
-        .renderBlockLoop\@:
-            _RENDER_BLOCK
-            dbra \patternNumber, .renderBlockLoop\@
-        Endm
 
         ; ---------------------------------------------------------------------------------------
         ; Start of sub routine _MapRenderRowBuffer
         ; ----------------
+        ; Register allocation:
+        ; - d0: Buffer mask for looping
+        ; - d1: Buffer offset
+        ; - d2: Map start column
+        ; - a0: width of the plane rendered
+        ; - a1: Address of first chunk in map
+        ; - a2: Base address of chunk table
+        ; - a3: Base address of block table
+        ; - a4: Base address of renderbuffer
 
         ; Load address registers
         movea.l loadedMap, a0
@@ -245,93 +376,18 @@ _RENDER_PARTIAL_CHUNK Macro patternNumber
         add.w   d6, d6
         add.w   d6, d6
 
-        ; Load address registers
-        lea     chunkTable, a2
-        lea     blockTable, a3
-        lea     mapRowBuffer, a4
-
-        ; If first block is full adjust DMA buffer offset
-        btst    #0, d1
-        bne     .initialBlockPartial
-        addq.l  #SIZE_WORD, a4
-
-    .initialBlockPartial:
-
         ; Buffer offset/rotation mask
         move.w  d1, d2                                              ; d2 = current map column
         move.w  (vdpMetrics + vdpPlaneWidthPatterns), d0
+        move.w  d0, a0                                              ; Store row size in a0
         subq.w  #1, d0
         add.w   d0, d0                                              ; d0 = buffer mask
-        andi.w  #~1, d1
         add.w   d1, d1                                              ; d1 = buffer offset
 
-        ; Render chunks
-
-        moveq   #0, d3
-        andi.w  #$0f, d2
-        beq     .fullChunks
-
-            ; ---------------------------------------------
-            ; Render first partial chunk
-
-            move.w  d2, d3
-            andi.w  #$0e, d3                                        ; d3 = offset of block column in chunk
-
-            _START_CHUNK d3
-
-            neg.w   d2
-            andi.w  #$0f, d2                                        ; d2 = number of patterns rendered
-
-            PUSHW   d2                                              ; Store for later use
-
-            _RENDER_PARTIAL_CHUNK d2
-
-            POPW    d3                                              ; d3 = number of patterns left to render
-
-            ; ---------------------------------------------
-            ; Render full chunks
-
-        .fullChunks:
-            move.w  (vdpMetrics + vdpPlaneWidthPatterns), d2
-            sub.w   d3, d2                                          ; d2 = remaining pattern columns
-
-            PUSHW d2                                                ; Store for later use
-
-            lsr.w   #4, d2                                          ; d2 = number of full chunks
-
-            subq.w  #1, d2
-        .fullChunkLoop:
-
-                _START_CHUNK
-
-                    _RENDER_BLOCK
-                    _RENDER_BLOCK
-                    _RENDER_BLOCK
-                    _RENDER_BLOCK
-                    _RENDER_BLOCK
-                    _RENDER_BLOCK
-                    _RENDER_BLOCK
-                    _RENDER_BLOCK
-
-            dbra d2, .fullChunkLoop
-
-            ; ---------------------------------------------
-            ; Render the last partial chunk
-
-            POPW d2                                                 ; d2 = number of patterns left to render
-
-            andi.w  #$0f, d2
-            beq     .done                                           ; Nothing left
-
-            _START_CHUNK
-
-                _RENDER_PARTIAL_CHUNK d2
-
-    .done:
+        _RENDER_BUFFER mapRowBuffer
 
         Purge _START_CHUNK
         Purge _RENDER_BLOCK
-        Purge _RENDER_PARTIAL_CHUNK
         rts
 
 
@@ -373,4 +429,130 @@ MapRenderColumn:
 ; - d1: Map start row
 ; Uses: d0-d7/a0-a6
 _MapRenderColumnBuffer:
+_START_CHUNK Macro rowOffset
+            move.w  (a1), d7                                        ; Chunk ref in d7
+            adda.w  a6, a1
+            move.w  d7, d5
+            lsl.w   #7, d5
+            lea     (a2, d5), a5
+            swap    d6                                              ; d6 = chunk column offset
+            move.w  d6, d5
+
+            If (narg = 1)
+                lsl.w   #3, \rowOffset
+                add.w  \rowOffset, d5
+            EndIf
+
+            moveq   #CHUNK_ROW_STRIDE, d4
+            btst    #CHUNK_REF_VFLIP, d7
+            beq     .chunkNotVFlipped\@
+            eor.w   #$0070, d5                                      ; Flip chunk row offset
+            neg.w   d4
+
+        .chunkNotVFlipped\@:
+            btst    #CHUNK_REF_HFLIP, d7
+            beq     .chunkNotHFlipped\@
+            eor.w   #$000e, d5                                      ; Flip chunk column offset
+
+        .chunkNotHFlipped\@:
+            adda.w  d5, a5                                          ; a5 = chunk block address
+            swap    d7
+            move.w  d4, d7                                          ; d7 = [chunk reference]:[block address increment]
+            swap    d6                                              ; d6 = block column index
+        Endm
+
+_RENDER_BLOCK Macro position
+            move.w  (a5), d3                                        ; d3 = block ref
+            adda.w  d7, a5
+            swap    d7
+            move.w  d3, d4
+            eor.w   d7, d3                                          ; d3 = current orientation
+            swap    d7
+            andi.w  #BLOCK_REF_INDEX_MASK, d4
+            lsl.w   #3, d4
+            add.w   d6, d4
+            btst    #BLOCK_REF_HFLIP, d3
+            beq     .blockNotHFlipped\@
+            eor.w   #$02, d4
+        .blockNotHFlipped\@:
+            move.w  (a3, d4), d5
+            move.w  4(a3, d4), d4
+            btst    #BLOCK_REF_VFLIP, d3
+            bne     .blockVFlipped\@
+            exg     d4, d5
+
+        .blockVFlipped\@:
+            add.w   d3, d3                                          ; Allign block+chunk orientation flags with pattern orientation flags
+            andi.w  #PATTERN_REF_ORIENTATION_MASK, d3
+
+            If ((narg = 0) | strcmp('\position', 'START'))
+                _RENDER_PATTERN
+            EndIf
+
+            If ((narg = 0) | strcmp('\position', 'END'))
+                move.w  d5, d4
+                _RENDER_PATTERN
+            EndIf
+        Endm
+
+        ; ---------------------------------------------------------------------------------------
+        ; Start of sub routine _MapRenderRowBuffer
+        ; ----------------
+        ; Register allocation:
+        ; - d0: Buffer mask for looping
+        ; - d1: Buffer offset
+        ; - d2: Map start row
+        ; - a0: height of the plane rendered
+        ; - a1: Address of first chunk in map
+        ; - a2: Base address of chunk table
+        ; - a3: Base address of block table
+        ; - a4: Base address of renderbuffer
+
+        ; Load address registers
+        movea.l loadedMap, a0
+        moveq   #0, d7
+        move.w  mapWidth(a0), d7
+        lea     mapRowOffsetTable(a0), a1                           ; a1 = row offset table
+        movea.l mapDataAddress(a0), a0                              ; a0 = map data
+
+        ; Store address of first chunk in a1
+        move.w  d0, d5
+        lsr.w   #4, d5
+        move.w  d1, d6
+        lsr.w   #4, d6
+        add.w   d6, d6
+        move.w  (a1, d6), d6
+        add.w   d5, d6
+        add.w   d5, d6
+        lea     (a0, d6), a1                                        ; a1 = address of chunk reference
+
+        ; Store map stride in a6
+        add.w   d7, d7
+        movea.l d7, a6
+
+        ; d6 = [chunk column offset]:[block column offset]
+        move.w  d0, d6
+        andi.w  #$0e, d6
+        swap    d6
+        move.w  d0, d6
+        andi.w  #$01, d6
+        add.w   d6, d6
+
+        ; Buffer offset/rotation mask
+        move.w  d1, d2                                              ; d2 = current map column
+        move.w  (vdpMetrics + vdpPlaneHeightPatterns), d0
+        movea.l d0, a0                                              ; a0 = Store plane width
+        subq.w  #1, d0
+        add.w   d0, d0                                              ; d0 = buffer mask
+        add.w   d1, d1                                              ; d1 = buffer offset
+
+        _RENDER_BUFFER mapColumnBuffer
+
+        Purge _START_CHUNK
+        Purge _RENDER_BLOCK
         rts
+
+
+    Purge _RENDER_PATTERN
+    Purge _RENDER_PARTIAL_CHUNK
+    Purge _RENDER_BUFFER
