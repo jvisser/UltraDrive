03d0afd457ab4a6a31cb6bd2aa1c3f414d8efeff
jvisser <jvisser3@gmail.com>
2020-10-31 15:19:29 +0100

    Add new tileset

--- game/assets/map/tilesets/meta/collisionblocks.tsx
+++ game/assets/map/tilesets/meta/collisionblocks.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="collisionblocks" tilewidth="16" tileheight="16" tilecount="9" columns="9">
- <image source="assets/collisionblocks.png" width="144" height="16"/>
+<tileset version="1.4" tiledversion="1.4.2" name="collisionblocks" tilewidth="16" tileheight="16" tilecount="33" columns="33">
+ <image source="assets/collisionblocks.png" width="528" height="16"/>
  <tile id="0" type="collisionblock"/>
  <tile id="1" type="collisionblock">
   <properties>
@@ -40,6 +40,122 @@
  <tile id="8" type="collisionblock">
   <properties>
    <property name="angle" type="float" value="75.964"/>
+  </properties>
+ </tile>
+ <tile id="9" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="82.875"/>
+  </properties>
+ </tile>
+ <tile id="10" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="82.875"/>
+  </properties>
+ </tile>
+ <tile id="11" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="82.875"/>
+  </properties>
+ </tile>
+ <tile id="12" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="82.875"/>
+  </properties>
+ </tile>
+ <tile id="13" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="82.875"/>
+  </properties>
+ </tile>
+ <tile id="14" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="82.875"/>
+  </properties>
+ </tile>
+ <tile id="15" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="82.875"/>
+  </properties>
+ </tile>
+ <tile id="16" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="82.875"/>
+  </properties>
+ </tile>
+ <tile id="17" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="26.565"/>
+  </properties>
+ </tile>
+ <tile id="18" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="26.565"/>
+  </properties>
+ </tile>
+ <tile id="19" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="14.036"/>
+  </properties>
+ </tile>
+ <tile id="20" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="14.036"/>
+  </properties>
+ </tile>
+ <tile id="21" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="14.036"/>
+  </properties>
+ </tile>
+ <tile id="22" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="14.036"/>
+  </properties>
+ </tile>
+ <tile id="23" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="7.125"/>
+  </properties>
+ </tile>
+ <tile id="24" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="7.125"/>
+  </properties>
+ </tile>
+ <tile id="25" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="7.125"/>
+  </properties>
+ </tile>
+ <tile id="26" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="7.125"/>
+  </properties>
+ </tile>
+ <tile id="27" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="7.125"/>
+  </properties>
+ </tile>
+ <tile id="28" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="7.125"/>
+  </properties>
+ </tile>
+ <tile id="29" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="7.125"/>
+  </properties>
+ </tile>
+ <tile id="30" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="7.125"/>
+  </properties>
+ </tile>
+ <tile id="31" type="collisionblock"/>
+ <tile id="32" type="collisionblock">
+  <properties>
+   <property name="angle" type="float" value="90"/>
   </properties>
  </tile>
 </tileset>
