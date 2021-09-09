b4799c2cdc2dd82827d9748b061c553d78286c10
jvisser <946768+jvisser@users.noreply.github.com>
2021-09-09 19:59:34 +0200

    Add support for md-profiler

--- game/src/build_and_run.sh
+++ game/src/build_and_run.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ $# -eq 0 ]; 
+if [ $# -eq 0 ];
     then
         echo 'No map filename provided. Exiting'
         exit 1
@@ -17,15 +17,27 @@ java -jar '../../tools/mapconvert/target/MapConvert.jar'    \
         -a './ultradrive/config/allocation.yaml'            \
         -r
 
+rm -f 'ultradrive.sym' 'ultradrive-tracelog.json'
+
 # Compile source
-if [ $# -eq 2 ]; 
+if [ $# -eq 2 ];
     then
     # Debug
-    asm68k //p //e "DEBUG='$2'" ./assembly.asm,boot.bin
+    asm68k //p //e "DEBUG='$2'" ./assembly.asm,ultradrive.bin,ultradrive.sym
 else
     # Normal
-    asm68k //p ./assembly.asm,boot.bin
+    asm68k //p ./assembly.asm,ultradrive.bin,ultradrive.sym
 fi
 
 # Run
-blastem -m gen boot.bin
+blastem -m gen ultradrive.bin
+
+# Generate trace log file
+if command -v 'md-profiler';
+    then
+    if [ -e 't' ];
+    then
+        md-profiler -s 'ultradrive.sym' -i 't' -o 'ultradrive-tracelog.json'
+        rm -f 't'
+    fi
+fi
